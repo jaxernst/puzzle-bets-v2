71e4ee506b8b810acfcaddcdfc2f08d5bae31998
jackson <jaxernst@gmail.com>
2024-06-23 14:30:22 -0700

    Add fonts + styling assets

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Arial", "sans-serif"],
+        angkor: ["Angkor"],
+      },
+      fontSize: {
+        angkor: ["14px", "15.4px"],
+      },
+      colors: {
+        "pb-yellow": "#FFC700",
+        "pb-green": "#2FB163",
+        "pb-orange": "#E17E22",
+        "pb-silver": "#E2E4F4",
+        "pb-gray-1": "#CCCCCC",
+        "pb-beige-1": "#FEECC8",
+        "pb-beige-2": "#CDC1A8",
+      },
+    },
   },
   plugins: [],
 }
