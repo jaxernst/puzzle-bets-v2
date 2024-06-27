a7a16905fa2a852ba0f9d927c53ca259bb88171a
jackson <jaxernst@gmail.com>
2024-06-26 22:31:20 -0700

    Add back button

--- packages/client/src/routes/game/GameHeader.svelte
+++ packages/client/src/routes/game/GameHeader.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import AnimatedArrow from "$lib/components/AnimatedArrow.svelte"
   import type { PuzzleType } from "$lib/types"
   import { capitalized } from "$lib/util"
 
@@ -7,11 +8,22 @@
 
 <div class="flex w-full justify-center">
   <div class="flex max-w-[1000px] flex-grow justify-between">
-    {#if gameId}
-      <div class="font-bold">{capitalized(puzzle)} | Live</div>
-    {:else}
-      <div class="font-bold">Practice {capitalized(puzzle)}</div>
-    {/if}
+    <div class="flex items-center">
+      <AnimatedArrow class="h-6 w-6" direction={"left"} />
+
+      {#if gameId}
+        <button
+          onclick={() => window.history.back()}
+          class="font-bold hover:underline">{capitalized(puzzle)} | Live</button
+        >
+      {:else}
+        <button
+          onclick={() => window.history.back()}
+          class="font-bold hover:underline"
+          >Practice {capitalized(puzzle)}</button
+        >
+      {/if}
+    </div>
 
     <div class="flex items-center gap-1 text-sm">
       <div class="rounded-full bg-black px-1.5 py-0.5 text-white">$0 Wager</div>
