4a3111aee51eb03e9f5593f87adc8502db09e456
jaxernst <51183683+jaxernst@users.noreply.github.com>
2024-08-02 15:03:54 -0700

    Style lobby game card

--- packages/client/src/routes/game-controller/PublicGamePreviewCard.svelte
+++ packages/client/src/routes/game-controller/PublicGamePreviewCard.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
   import { type Game } from "$lib/types"
-  import { entityToInt } from "$lib/util"
+  import { entityToInt, shortenAddress } from "$lib/util"
+  import { displayNameStore } from "$lib/displayNameStore.svelte"
+  import { user } from "$lib/userStore.svelte"
+
   let { game } = $props<{ game: Game }>()
+
+  let creatorDisplayName = $derived(displayNameStore.get(game.p1, false))
+
+  let isUserGame = $derived(user.address === game.p1)
 </script>
 
 <div
-  class="flex w-[343px] flex-col gap-[10px] rounded bg-white p-4 text-[13px] leading-none"
+  class="mb-[5px] flex w-[343px] flex-col gap-3 rounded bg-white p-4 text-[13px] leading-none"
   style={"box-shadow: 0px 5px 0px 0px #E3DDCD;"}
 >
-  <div class="flex justify-between">
+  <div class="flex items-center justify-between">
     <div>
       #{entityToInt(game.id)}
       <div class="font-bold">Wordle</div>
@@ -22,28 +29,37 @@
   </div>
 
   <div class="flex flex-col">
-    <div class="self-end text-xs text-[#757575]">Amount to Bet</div>
-
     <div class="flex items-center justify-between">
-      <div class="flex gap-1">
+      <div class="flex items-center gap-1.5">
         <img
           alt="Player Avatar"
-          src={"/character1.png"}
-          class="h-[20px] w-[20px]"
+          src={"/avatar1.png"}
+          class="h-[26px] w-[26px]"
         />
-        <div>0x123...456</div>
+        <div class="flex flex-col gap-1">
+          <div class="font-bold">{creatorDisplayName}</div>
+          <div>{shortenAddress(game.p1)}</div>
+        </div>
       </div>
 
-      <div class=" flex gap-1 font-bold">
-        <div>$5.00</div>
-        <div class="text-[#8F8F8F]">(.00128 ETH)</div>
+      <div class="flex flex-col items-end gap-1">
+        <div class="text-xs text-[#757575]">Amount to Bet</div>
+        <div class="flex gap-1 font-bold">
+          <div>$5.00</div>
+          <div class="text-[#8F8F8F]">(.00128 ETH)</div>
+        </div>
       </div>
     </div>
   </div>
 
   <button
-    class="self-stretch rounded bg-black p-3 text-center text-base text-white"
+    class="self-stretch rounded bg-black p-3 text-center text-base font-bold text-white disabled:opacity-40"
+    disabled={isUserGame}
   >
-    Join
+    {#if isUserGame}
+      Your Game - Waiting for opponent
+    {:else}
+      Join
+    {/if}
   </button>
 </div>
