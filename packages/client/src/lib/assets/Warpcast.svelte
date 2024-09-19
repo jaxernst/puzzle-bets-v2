0a544ee16cf2f7ecd56354b5652fa48b79b848c3
jackson <jaxernst@gmail.com>
2024-09-18 22:52:19 -0700

    Svg logos

---
+++ packages/client/src/lib/assets/Warpcast.svelte
@@ -0,0 +1,36 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="32"
+  height="32"
+  viewBox="0 0 32 32"
+  fill="none"
+>
+  <g clip-path="url(#clip0_251_4375)">
+    <mask
+      id="mask0_251_4375"
+      style="mask-type:luminance"
+      maskUnits="userSpaceOnUse"
+      x="0"
+      y="0"
+      width="32"
+      height="32"
+    >
+      <path d="M31.9901 0H0V31.9901H31.9901V0Z" fill="white" />
+    </mask>
+    <g mask="url(#mask0_251_4375)">
+      <path
+        d="M24.0698 31.9902H7.92028C3.55304 31.9902 0 28.4374 0 24.07V7.92064C0 3.5533 3.55304 0.000244141 7.92028 0.000244141H24.0698C28.4371 0.000244141 31.9901 3.5533 31.9901 7.92064V24.07C31.9901 28.4374 28.4371 31.9902 24.0698 31.9902Z"
+        fill="#472A91"
+      />
+      <path
+        d="M20.9907 10.124L19.4134 16.048L17.8309 10.124H14.1891L12.5914 16.0916L10.999 10.124H6.85107L10.7054 23.2219H14.2839L15.995 17.1397L17.7061 23.2219H21.2923L25.1382 10.124H20.9907Z"
+        fill="white"
+      />
+    </g>
+  </g>
+  <defs>
+    <clipPath id="clip0_251_4375">
+      <rect width="32" height="32" fill="white" />
+    </clipPath>
+  </defs>
+</svg>
