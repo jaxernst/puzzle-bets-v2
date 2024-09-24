7dff9b7ccc73449d3d2d63ee5c5becbe73e81bfd
jackson <jaxernst@gmail.com>
2024-09-23 21:43:40 -0700

    Try safari theme fix

--- packages/client/src/routes/+page.svelte
+++ packages/client/src/routes/+page.svelte
@@ -40,11 +40,11 @@
   >
     <div class="overflow-none flex items-center gap-16 sm:hidden">
       <img
-        class="w-[120px] -translate-x-4"
+        class="w-[125px] -translate-x-4"
         src="/character2.png"
         alt="Puzzle Character 2"
       />
-      <img class="w-[170px]" src="/character1.png" alt="Puzzle Character 1" />
+      <img class="w-[180px]" src="/character1.png" alt="Puzzle Character 1" />
     </div>
 
     <div class="flex flex-col items-center gap-2 sm:gap-4">
