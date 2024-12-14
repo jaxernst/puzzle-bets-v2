fa0698a2e0e2f917bf90ccc1316ae5a33e3ab09a
jackson <jaxernst@gmail.com>
2024-12-13 19:43:25 -0800

    Draft fee split

---
+++ packages/contracts/src/library/FeeReciever.sol
@@ -0,0 +1,12 @@
+// Contract to hide and forward funds to addresses
+
+/*
+ - Addresses can be added to the split at any time by the admin
+ - Admin permission can be burned
+ - Two functions:
+  - set() -> takes in array of all address -> percentage amounts 
+  - receive() -> takes in the value and distrbutres it among the split addresses
+*/
+contract FeeReciever {
+
+}
