4fa0353dcd56628b5de1b2d3fb12838fb17e4c96
jackson <jaxernst@gmail.com>
2025-01-14 21:20:09 -0800

    IMporve redirect logic to navigate directly into a live game

--- packages/client/src/routes/game/wordle/[gameId]/+page.svelte
+++ packages/client/src/routes/game/wordle/[gameId]/+page.svelte
@@ -1,22 +1,16 @@
 <script>
   import { page } from "$app/stores"
   import { gameIdToGame, isGamePlayer, playerFields } from "$lib/gameQueries"
+  import { frameStore } from "$lib/farcaster/frameStore.svelte"
   import { mud } from "$lib/mudStore.svelte"
   import { goto } from "$app/navigation"
   import { user } from "$lib/userStore.svelte"
   import { intToEntity } from "$lib/util"
   import LiveGame from "$lib/game-components/LiveGame.svelte"
+  import DotLoader from "$lib/components/DotLoader.svelte"
+  import { walletStore } from "$lib/walletStore.svelte"
 
   let gameEntity = intToEntity($page.params.gameId)
-
-  const isPlayer = () =>
-    user.address && gameEntity && isGamePlayer(user.address, gameEntity, mud)
-
-  $effect(() => {
-    if (!isPlayer()) {
-      goto("/dashboard")
-    }
-  })
 
   let game = $derived.by(() => {
     if (
@@ -35,7 +29,30 @@
       ...playerFields(game, user.address),
     }
   })
+
+  $effect(() => {
+    const frameStoreChecked = frameStore.unavailable || frameStore.initialized
+    const noUser = frameStoreChecked && !walletStore.connecting && !user.address
+    if (noUser || (mud.synced && !game)) {
+      goto("/dashboard")
+    }
+  })
+
+
+  $effect(() => {
+    setTimeout(() => {
+      if (!mud.synced) {
+        goto("/dashboard")
+      }
+    }, 6000)
+  })
 </script>
+
+{#if !mud.synced}
+  <div class="flex h-screen flex-col items-center justify-center">
+    <DotLoader class="h-10 w-10" />
+  </div>
+{/if}
 
 {#if game && user.address}
   <LiveGame {game} user={user.address} />
