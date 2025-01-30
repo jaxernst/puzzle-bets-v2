a2f979a16b04614d18e8da47a4958ce11d51d996
jaxernst <51183683+jaxernst@users.noreply.github.com>
2025-01-30 11:11:21 -0800

    Add mainnet as supported chain

--- packages/client/src/lib/mud/supportedChains.ts
+++ packages/client/src/lib/mud/supportedChains.ts
@@ -1,32 +1,4 @@
 import { type MUDChain, mudFoundry } from "@latticexyz/common/chains"
 import { base, baseSepolia, type Chain } from "viem/chains"
 
-// Override default lattice chain with 'miner.testnet' (from follower.testnet)
-export const latticeTestnet = {
-  name: "Lattice Testnet",
-  id: 4242,
-  nativeCurrency: { decimals: 18, name: "Ether", symbol: "ETH" },
-  rpcUrls: {
-    default: {
-      http: ["https://miner.testnet-chain.linfra.xyz"],
-      webSocket: ["wss://miner.testnet-chain.linfra.xyz"],
-    },
-    public: {
-      http: ["https://miner.testnet-chain.linfra.xyz"],
-      webSocket: ["wss://miner.testnet-chain.linfra.xyz"],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: "Otterscan",
-      url: "https://explorer.testnet-chain.linfra.xyz",
-    },
-  },
-  faucetUrl: "https://faucet.testnet-mud-services.linfra.xyz",
-} as const satisfies MUDChain
-
-/*
- * See https://mud.dev/tutorials/minimal/deploy#run-the-user-interface
- * for instructions on how to add networks.
- */
-export const supportedChains: Array<Chain> = [mudFoundry, baseSepolia]
+export const supportedChains: Array<Chain> = [mudFoundry, baseSepolia, base]
