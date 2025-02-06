e279fb6278d3a47d94cd6957fe15bea0a2343d72
jaxernst <51183683+jaxernst@users.noreply.github.com>
2025-02-06 11:12:32 -0800

    Remove webhook redirect

--- packages/client/src/routes/api/webhook/+server.ts
+++
@@ -1,6 +0,0 @@
-import { redirect } from "@sveltejs/kit"
-
-// Temp fix: Warpcast frames is not using the webhook url specifified in farcaster.json
-export const POST = () => {
-  return redirect(302, "https://puzzlebets.xyz/api/farcaster/webhook")
-}
